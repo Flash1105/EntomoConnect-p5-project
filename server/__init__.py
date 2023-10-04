bbec29d85eb19138724e2bb72f4c12674fa41207
Flash1105-2 <flashpay110599@gmail.com>
2023-10-04 01:01:23 -0400

    important imports

--- server/__init__.py
+++ server/__init__.py
@@ -2,18 +2,24 @@ from flask import Flask
 from flask_login import LoginManager
 from .config import Config
 from .database import db
-from flask import Blueprint
 
 
 login_manager = LoginManager()
-bp = Blueprint('auth', __name__)
+
 
 @login_manager.user_loader
 def load_user(user_id):
     from server.models.user import User 
     return User.query.get(int(user_id))
 
+
+import sys
+import os
+
 def create_app(config_class=Config):
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+    sys.path.insert (0, project_root)
     template_path = '/home/flash1105/development/code/phase-5/python-p5-project-EntomoConnect/templates'
     app = Flask(__name__, template_folder=template_path)
     app.config.from_object(config_class)
